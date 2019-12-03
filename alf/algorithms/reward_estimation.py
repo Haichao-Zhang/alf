338d4cc797db7871be6e5c4ac174e2843f78d145
Haichao Zhang <hczhang1@gmail.com>
2019-12-03 14:55:10 -0800

    reward from state

--- alf/algorithms/reward_estimation.py
+++ alf/algorithms/reward_estimation.py
@@ -269,3 +269,186 @@ class RewardAlgorithm(Algorithm):
     def calc_loss(self, info: ICMInfo):
         loss = tf.nest.map_structure(tf.reduce_mean, info.loss)
         return LossInfo(scalar_loss=loss.loss, extra=loss.extra)
+
+    #=================Pure state based reward estimation
+
+
+@gin.configurable
+class RewardAlgorithmState(Algorithm):
+    """Intrinsic Curiosity Module
+
+    This module generate the intrinsic reward based on predition error of
+    observation.
+
+    See Pathak et al "Curiosity-driven Exploration by Self-supervised Prediction"
+    """
+
+    def __init__(self,
+                 action_spec,
+                 feature_spec,
+                 hidden_size=256,
+                 reward_adapt_speed=8.0,
+                 encoding_net: Network = None,
+                 forward_net: Network = None,
+                 inverse_net: Network = None,
+                 fuse_net: Network = None,
+                 name="ICMAlgorithm"):
+        """Create an ICMAlgorithm.
+
+        Args:
+            hidden_size (int|tuple): size of hidden layer(s)
+            reward_adapt_speed (float): how fast to adapt the reward normalizer.
+                rouphly speaking, the statistics for the normalization is
+                calculated mostly based on the most recent T/speed samples,
+                where T is the total number of samples.
+            encoding_net (Network): network for encoding observation into a
+                latent feature specified by feature_spec. Its input is same as
+                the input of this algorithm.
+            forward_net (Network): network for predicting next feature based on
+                previous feature and action. It should accept input with spec
+                [feature_spec, encoded_action_spec] and output a tensor of shape
+                feature_spec. For discrete action, encoded_action is an one-hot
+                representation of the action. For continuous action, encoded
+                action is same as the original action.
+            fuse_net (Network): from distance to reward.
+        """
+        super(RewardAlgorithmState, self).__init__(
+            train_state_spec=feature_spec, name=name)
+
+        flat_action_spec = tf.nest.flatten(action_spec)
+        assert len(
+            flat_action_spec) == 1, "ICM doesn't suport nested action_spec"
+
+        print(feature_spec)
+
+        flat_feature_spec = tf.nest.flatten(feature_spec)
+        assert len(
+            flat_feature_spec) == 1, "ICM doesn't support nested feature_spec"
+
+        action_spec = flat_action_spec[0]
+
+        if tensor_spec.is_discrete(action_spec):
+            self._num_actions = action_spec.maximum - action_spec.minimum + 1
+        else:
+            self._num_actions = action_spec.shape[-1]
+
+        self._action_spec = action_spec
+
+        feature_dim = flat_feature_spec[0].shape[-1]
+
+        self.feature_dim = feature_dim
+
+        self._encoding_net = encoding_net
+
+        if isinstance(hidden_size, int):
+            hidden_size = (hidden_size, )
+
+        if forward_net is None:
+            encoded_action_spec = tensor_spec.TensorSpec((self._num_actions, ),
+                                                         dtype=tf.float32)
+            forward_net = EncodingNetwork(
+                name="forward_net",
+                input_tensor_spec=[feature_spec, encoded_action_spec],
+                fc_layer_params=hidden_size,
+                last_layer_size=feature_dim)
+        self._forward_net = forward_net
+
+        if inverse_net is None:
+            inverse_net = EncodingNetwork(
+                name="inverse_net",
+                input_tensor_spec=[feature_spec, feature_spec],
+                fc_layer_params=hidden_size,
+                last_layer_size=self._num_actions,
+                last_kernel_initializer=tf.initializers.Zeros())
+
+        self._inverse_net = inverse_net
+        # if fuse_net is None:
+        #     fuse_net = EncodingNetwork(
+        #         name="inverse_net",
+        #         input_tensor_spec=[feature_spec, feature_spec],
+        #         fc_layer_params=hidden_size,
+        #         last_layer_size=self._num_actions,
+        #         last_kernel_initializer=tf.initializers.Zeros())
+
+        self._fuse_net = fuse_net
+
+        self._reward_normalizer = ScalarAdaptiveNormalizer(
+            speed=reward_adapt_speed)
+
+    def _encode_action(self, action):
+        if tensor_spec.is_discrete(self._action_spec):
+            return tf.one_hot(indices=action, depth=self._num_actions)
+        else:
+            return action
+
+    def train_step(self, inputs, state, calc_intrinsic_reward=True):
+        """
+        Args:
+            inputs (tuple): observation and previous action
+            state (Tensor):  state for ICM (previous feature)
+            calc_intrinsic_reward (bool): if False, only return the losses
+        Returns:
+            TrainStep:
+                outputs: empty tuple ()
+                state: empty tuple ()
+                info (ICMInfo):
+        """
+        feature, prev_action, reward_external = inputs
+
+        prev_action = tf.math.floormod(prev_action, 3.142)  # mod operation
+
+        # if self._encoding_net is not None:
+        #     goal_feature, _, self_state_feature = self._encoding_net(feature)
+        state_goal = feature['image']
+        state_self = feature['state']
+
+        prev_feature = state
+        prev_action = self._encode_action(prev_action)
+
+        # the reward estimation module should estimate next state and reward jointly (a complete forward model)
+
+        # reward prediction loss based on forward_pred
+        #self_pose = state_self  # all position based state now, no need to split
+
+        # reward estimation based on one-step forward prediction
+        reward_input_feature = tf.concat(
+            [
+                state_self,
+                state_goal,  # train goal encoding
+            ],
+            axis=1)
+
+        reward_pred, _ = self._fuse_net(reward_input_feature)
+
+        binary_mask = tf.dtypes.cast(
+            tf.math.greater(reward_external, tf.zeros_like(reward_external)),
+            tf.float32)
+
+        scaled_mask = binary_mask * 1 + 1e-1 * (1 - binary_mask)
+
+        #masked_reward = tf.multiply(scaled_mask, reward_external)
+
+        pred_reward_mse = 0.5 * tf.square(
+            reward_pred - reward_external)  # reduce the last dim
+        reward_loss = 0.5 * tf.multiply(
+            pred_reward_mse,
+            tf.stop_gradient(scaled_mask))  # reduce the last dim
+
+        intrinsic_reward = ()
+        if calc_intrinsic_reward:
+            # intrinsic_reward = tf.stop_gradient(forward_loss)
+            intrinsic_reward = tf.stop_gradient(reward_pred)
+            # intrinsic_reward = self._reward_normalizer.normalize(
+            #     intrinsic_reward)
+
+        return AlgorithmStep(
+            outputs=(),
+            state=state_self,
+            info=ICMInfo(
+                reward=intrinsic_reward,
+                loss=LossInfo(
+                    loss=reward_loss, extra=dict(reward_loss=reward_loss))))
+
+    def calc_loss(self, info: ICMInfo):
+        loss = tf.nest.map_structure(tf.reduce_mean, info.loss)
+        return LossInfo(scalar_loss=loss.loss, extra=loss.extra)
