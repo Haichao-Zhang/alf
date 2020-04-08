02fc498c2589a9bf87c0669b8e7516fa0836522a
Haichao Zhang <hczhang1@gmail.com>
2020-04-08 11:38:53 -0700

    switch initializer back

--- alf/networks/encoding_networks.py
+++ alf/networks/encoding_networks.py
@@ -321,16 +321,16 @@ class EncodingNetwork(Network):
             name=name)
 
         if kernel_initializer is None:
-            kernel_initializer = torch.nn.init.xavier_uniform_
+            #kernel_initializer = torch.nn.init.xavier_uniform_
             # gain = _numerical_calculate_gain(activation)
             # kernel_initializer = functools.partial(
             #     torch.nn.init.xavier_uniform_, gain=gain)
 
-            # kernel_initializer = functools.partial(
-            #     variance_scaling_init,
-            #     mode='fan_in',
-            #     distribution='truncated_normal',
-            #     nonlinearity=activation)
+            kernel_initializer = functools.partial(
+                variance_scaling_init,
+                mode='fan_in',
+                distribution='truncated_normal',
+                nonlinearity=activation)
 
         self._img_encoding_net = None
         if conv_layer_params:
