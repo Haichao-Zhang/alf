f147752337183319f4ff8fc7e19429af001afd77
Haichao Zhang <hczhang1@gmail.com>
2020-04-09 14:16:16 -0700

    reward learning train with bug fix in reward estimation net last activation

--- alf/algorithms/mbrl_algorithm.py
+++ alf/algorithms/mbrl_algorithm.py
@@ -34,7 +34,8 @@ from alf.utils import losses, common, dist_utils, tensor_utils
 from alf.utils.math_ops import add_ignore_empty
 
 from alf.algorithms.dynamics_learning_algorithm import DynamicsLearningAlgorithm
-from alf.algorithms.reward_learning_algorithm import RewardEstimationAlgorithm
+# from alf.algorithms.reward_learning_algorithm import RewardEstimationAlgorithm
+from alf.algorithms.reward_learning_algorithm_2 import RewardEstimationAlgorithm
 from alf.algorithms.planning_algorithm import PlanAlgorithm
 
 MbrlState = namedtuple("MbrlState", ["dynamics", "reward", "planner"])
@@ -216,8 +217,6 @@ class MbrlAlgorithm(OffPolicyAlgorithm):
     def calc_loss(self, training_info: TrainingInfo):
         loss = training_info.info.dynamics.loss
 
-        #loss_reward = training_info.info.reward.loss
-
         # loss = add_ignore_empty(loss, training_info.info.reward)
         # loss = add_ignore_empty(loss, loss_planner)
         # return LossInfo(loss=loss.loss, extra=())
@@ -235,6 +234,7 @@ class MbrlAlgorithm(OffPolicyAlgorithm):
                 extra=MbrlLossInfo(
                     dynamics=loss.extra, planner=loss_planner.extra))
         else:
+            loss_reward = training_info.info.reward.loss
             MbrlLossInfo = namedtuple('MbrlLossInfo',
                                       ("dynamics", "reward", "planner"))
             loss_planner = self._planner_module.calc_loss(
