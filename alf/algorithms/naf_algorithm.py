2a1348174d9ebb70b1a231b239687d0470f5652b
Haichao Zhang <hczhang1@gmail.com>
2020-04-10 15:01:04 -0700

    done with critic networks

---
+++ alf/algorithms/naf_algorithm.py
@@ -0,0 +1,237 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Normalized Advantage Function (NAF)."""
+
+import numpy as np
+import gin
+
+import torch
+import torch.nn as nn
+import torch.distributions as td
+from typing import Callable
+
+from alf.algorithms.config import TrainerConfig
+from alf.algorithms.off_policy_algorithm import OffPolicyAlgorithm
+from alf.algorithms.one_step_loss import OneStepTDLoss
+from alf.algorithms.rl_algorithm import RLAlgorithm
+from alf.data_structures import TimeStep, Experience, LossInfo, namedtuple
+from alf.data_structures import AlgStep, TrainingInfo
+from alf.nest import nest
+from alf.networks import ActorNetwork, CriticNetwork
+from alf.tensor_specs import TensorSpec, BoundedTensorSpec
+from alf.utils import losses, common, dist_utils, spec_utils
+
+NafCriticState = namedtuple("NafCriticState", ['critic', 'target_critic'])
+NafCriticInfo = namedtuple("NafCriticInfo", ["q_value", "target_q_value"])
+NafState = namedtuple("NafState", ['critic'])
+NafInfo = namedtuple("NafInfo", ["critic"], default_value=())
+DNafLossInfo = namedtuple('NafLossInfo', ('critic'))
+
+
+@gin.configurable
+class NafAlgorithm(OffPolicyAlgorithm):
+    """Normalized Advantage Function (NAF).
+
+    Reference:
+    Gu et al "Continuous Deep Q-Learning with Model-based Acceleration"
+    https://arxiv.org/pdf/1603.00748.pdf
+    """
+
+    def __init__(self,
+                 observation_spec,
+                 action_spec: BoundedTensorSpec,
+                 critic_network: CriticNetwork,
+                 env=None,
+                 config: TrainerConfig = None,
+                 ou_stddev=0.2,
+                 ou_damping=0.15,
+                 critic_loss=None,
+                 target_update_tau=0.05,
+                 target_update_period=1,
+                 dqda_clipping=None,
+                 actor_optimizer=None,
+                 critic_optimizer=None,
+                 gradient_clipping=None,
+                 debug_summaries=False,
+                 name="DdpgAlgorithm"):
+        """Create a DdpgAlgorithm.
+
+        Args:
+            action_spec (nested BoundedTensorSpec): representing the actions.
+            critic_network (Network): The network will be called with
+                call(observation, action).
+            env (Environment): The environment to interact with. env is a batched
+                environment, which means that it runs multiple simulations
+                simultateously. env only needs to be provided to the root
+                Algorithm.
+            config (TrainerConfig): config for training. config only needs to be
+                provided to the algorithm which performs `train_iter()` by
+                itself.
+            ou_stddev (float): Standard deviation for the Ornstein-Uhlenbeck
+                (OU) noise added in the default collect policy.
+            ou_damping (float): Damping factor for the OU noise added in the
+                default collect policy.
+            critic_loss (None|OneStepTDLoss): an object for calculating critic
+                loss. If None, a default OneStepTDLoss will be used.
+            target_update_tau (float): Factor for soft update of the target
+                networks.
+            target_update_period (int): Period for soft update of the target
+                networks.
+            dqda_clipping (float): when computing the actor loss, clips the
+                gradient dqda element-wise between [-dqda_clipping, dqda_clipping].
+                Does not perform clipping if dqda_clipping == 0.
+            actor_optimizer (torch.optim.optimizer): The optimizer for actor.
+            critic_optimizer (torch.optim.optimizer): The optimizer for critic.
+            gradient_clipping (float): Norm length to clip gradients.
+            debug_summaries (bool): True if debug summaries should be created.
+            name (str): The name of this algorithm.
+        """
+        train_state_spec = NafState(
+            critic=NafCriticState(
+                critic=critic_network.state_spec,
+                target_critic=critic_network.state_spec))
+
+        super().__init__(
+            observation_spec,
+            action_spec,
+            train_state_spec=train_state_spec,
+            env=env,
+            config=config,
+            gradient_clipping=gradient_clipping,
+            debug_summaries=debug_summaries,
+            name=name)
+
+        if critic_optimizer is not None:
+            self.add_optimizer(critic_optimizer, [critic_network])
+
+        self._critic_network = critic_network
+        self._target_critic_network = critic_network.copy()
+
+        self._ou_stddev = ou_stddev
+        self._ou_damping = ou_damping
+
+        if critic_loss is None:
+            critic_loss = OneStepTDLoss(debug_summaries=debug_summaries)
+        self._critic_loss = critic_loss
+
+        self._ou_process = common.create_ou_process(action_spec, ou_stddev,
+                                                    ou_damping)
+
+        self._update_target = common.get_target_updater(
+            models=[self._critic_network],
+            target_models=[self._target_critic_network],
+            tau=target_update_tau,
+            period=target_update_period)
+
+        self._dqda_clipping = dqda_clipping
+
+    def predict_step(self, time_step: TimeStep, state, epsilon_greedy=1.):
+        action, state = self._actor_network(
+            time_step.observation, state=state.actor.actor)
+        empty_state = nest.map_structure(lambda x: (), self.train_state_spec)
+
+        def _sample(a, ou):
+            if torch.rand(1) < epsilon_greedy:
+                return a + ou()
+            else:
+                return a
+
+        noisy_action = nest.map_structure(_sample, action, self._ou_process)
+        noisy_action = nest.map_structure(spec_utils.clip_to_spec,
+                                          noisy_action, self._action_spec)
+        state = empty_state._replace(
+            actor=DdpgActorState(actor=state, critic=()))
+        return AlgStep(
+            output=noisy_action,
+            state=state,
+            info=DdpgInfo(action_distribution=action))
+
+    def rollout_step(self, time_step: TimeStep, state=None):
+        if self.need_full_rollout_state():
+            raise NotImplementedError("Storing RNN state to replay buffer "
+                                      "is not supported by DdpgAlgorithm")
+        return self.predict_step(time_step, state, epsilon_greedy=1.0)
+
+    def _critic_train_step(self, exp: Experience, state: DdpgCriticState):
+        target_action, target_actor_state = self._target_actor_network(
+            exp.observation, state=state.target_actor)
+        target_q_value, target_critic_state = self._target_critic_network(
+            (exp.observation, target_action), state=state.target_critic)
+
+        q_value, critic_state = self._critic_network(
+            (exp.observation, exp.action), state=state.critic)
+
+        state = DdpgCriticState(
+            critic=critic_state,
+            target_actor=target_actor_state,
+            target_critic=target_critic_state)
+
+        info = DdpgCriticInfo(q_value=q_value, target_q_value=target_q_value)
+
+        return state, info
+
+    def _actor_train_step(self, exp: Experience, state: DdpgActorState):
+        action, actor_state = self._actor_network(
+            exp.observation, state=state.actor)
+
+        q_value, critic_state = self._critic_network((exp.observation, action),
+                                                     state=state.critic)
+
+        dqda = nest.pack_sequence_as(
+            action,
+            list(torch.autograd.grad(q_value.sum(), nest.flatten(action))))
+
+        def actor_loss_fn(dqda, action):
+            if self._dqda_clipping:
+                dqda = torch.clamp(dqda, -self._dqda_clipping,
+                                   self._dqda_clipping)
+            loss = 0.5 * losses.element_wise_squared_loss(
+                (dqda + action).detach(), action)
+            loss = loss.sum(list(range(1, loss.ndim)))
+            return loss
+
+        actor_loss = nest.map_structure(actor_loss_fn, dqda, action)
+        state = DdpgActorState(actor=actor_state, critic=critic_state)
+        info = LossInfo(loss=sum(nest.flatten(actor_loss)), extra=actor_loss)
+        return AlgStep(output=action, state=state, info=info)
+
+    def train_step(self, exp: Experience, state: DdpgState):
+        critic_state, critic_info = self._critic_train_step(
+            exp=exp, state=state.critic)
+        policy_step = self._actor_train_step(exp=exp, state=state.actor)
+        return policy_step._replace(
+            state=DdpgState(actor=policy_step.state, critic=critic_state),
+            info=DdpgInfo(
+                action_distribution=policy_step.output,
+                critic=critic_info,
+                actor_loss=policy_step.info))
+
+    def calc_loss(self, training_info: TrainingInfo):
+        critic_loss = self._critic_loss(
+            training_info=training_info,
+            value=training_info.info.critic.q_value,
+            target_value=training_info.info.critic.target_q_value)
+
+        actor_loss = training_info.info.actor_loss
+
+        return LossInfo(
+            loss=critic_loss.loss + actor_loss.loss,
+            extra=DdpgLossInfo(
+                critic=critic_loss.extra, actor=actor_loss.extra))
+
+    def after_update(self, training_info):
+        self._update_target()
+
+    def _trainable_attributes_to_ignore(self):
+        return ['_target_actor_network', '_target_critic_network']
---
+++ alf/networks/naf_critic_networks.py
@@ -0,0 +1,197 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""CriticNetworks"""
+
+import gin
+import functools
+import math
+
+import torch
+import torch.nn as nn
+
+import alf.utils.math_ops as math_ops
+import alf.nest as nest
+from alf.networks import Network, EncodingNetwork, LSTMEncodingNetwork
+from alf.networks.initializers import variance_scaling_init
+from alf.tensor_specs import TensorSpec
+
+
+@gin.configurable
+class NafCriticNetwork(Network):
+    """Create an instance of NafCriticNetwork."""
+
+    def __init__(self,
+                 input_tensor_spec,
+                 observation_input_processors=None,
+                 observation_preprocessing_combiner=None,
+                 observation_conv_layer_params=None,
+                 observation_fc_layer_params=None,
+                 action_fc_layer_params=None,
+                 joint_fc_layer_params=None,
+                 activation=torch.relu,
+                 kernel_initializer=None,
+                 use_last_kernel_initializer=True,
+                 last_activation=math_ops.identity,
+                 name="CriticNetwork"):
+        """Creates an instance of `NafCriticNetwork` for estimating action-value of
+        continuous actions. The action-value is defined as the expected return
+        starting from the given input observation and taking the given action.
+        This module takes observation as input and action as input and outputs
+        an action-value tensor with the shape of [batch_size].
+
+        Currently there seems no need for this class to handle nested inputs;
+        If necessary, extend the argument list to support it in the future.
+
+        Args:
+            input_tensor_spec: A tuple of TensorSpecs (observation_spec, action_spec)
+                representing the inputs.
+            observation_input_preprocessors (nested InputPreprocessor): a nest of
+                `InputPreprocessor`, each of which will be applied to the
+                corresponding observation input.
+            observation_preprocessing_combiner (NestCombiner): preprocessing called
+                on complex observation inputs.
+            observation_conv_layer_params (tuple[tuple]): a tuple of tuples where each
+                tuple takes a format `(filters, kernel_size, strides, padding)`,
+                where `padding` is optional.
+            observation_fc_layer_params (tuple[int]): a tuple of integers representing
+                hidden FC layer sizes for observations.
+            action_fc_layer_params (tuple[int]): a tuple of integers representing
+                hidden FC layer sizes for actions.
+            joint_fc_layer_params (tuple[int]): a tuple of integers representing
+                hidden FC layer sizes FC layers after merging observations and
+                actions.
+            activation (nn.functional): activation used for hidden layers. The
+                last layer will not be activated.
+            kernel_initializer (Callable): initializer for all the layers but
+                the last layer. If none is provided a variance_scaling_initializer
+                with uniform distribution will be used.
+            name (str):
+        """
+        super().__init__(
+            input_tensor_spec, skip_input_preprocessing=True, name=name)
+
+        if kernel_initializer is None:
+            kernel_initializer = functools.partial(
+                variance_scaling_init,
+                gain=math.sqrt(1.0 / 3),
+                mode='fan_in',
+                distribution='uniform')
+
+        observation_spec, action_spec = input_tensor_spec
+
+        flat_action_spec = nest.flatten(action_spec)
+        if len(flat_action_spec) > 1:
+            raise ValueError(
+                'Only a single action is supported by this network')
+
+        self._single_action_spec = flat_action_spec[0]
+        action_dim = action_spec.shape[0]
+
+        # [obs_dim -> hidden_dim]
+        self._obs_encoder = EncodingNetwork(
+            observation_spec,
+            input_preprocessors=observation_input_processors,
+            preprocessing_combiner=observation_preprocessing_combiner,
+            conv_layer_params=observation_conv_layer_params,
+            fc_layer_params=observation_fc_layer_params,
+            activation=activation,
+            kernel_initializer=kernel_initializer)
+
+        if use_last_kernel_initializer:
+            last_kernel_initializer = functools.partial(
+                torch.nn.init.uniform_, a=-0.003, b=0.003)
+        else:
+            last_kernel_initializer = None
+
+        # [hidden_dim -> action_dim]
+        # might need action squash
+        self._mu = EncodingNetwork(
+            TensorSpec((self._obs_encoder.output_spec.shape[0])),
+            fc_layer_params=joint_fc_layer_params,
+            activation=activation,
+            kernel_initializer=kernel_initializer,
+            last_layer_size=action_dim,
+            last_activation=last_activation,
+            last_kernel_initializer=last_kernel_initializer)
+
+        # [hidden_dim -> 1]
+        # TODO joint_fc_layer_params change to non-shared
+        self._V = EncodingNetwork(
+            TensorSpec((self._obs_encoder.output_spec.shape[0])),
+            fc_layer_params=joint_fc_layer_params,
+            activation=activation,
+            kernel_initializer=kernel_initializer,
+            last_layer_size=1,
+            last_activation=last_activation,
+            last_kernel_initializer=last_kernel_initializer)
+
+        self._L = EncodingNetwork(
+            TensorSpec((self._obs_encoder.output_spec.shape[0])),
+            fc_layer_params=joint_fc_layer_params,
+            activation=activation,
+            kernel_initializer=kernel_initializer,
+            last_layer_size=action_dim**2,
+            last_activation=last_activation,
+            last_kernel_initializer=last_kernel_initializer)
+
+        self.tril_mask = torch.tril(
+            torch.ones(action_dim, num_outputs), diagonal=-1).unsqueeze(0)
+        self.diag_mask = torch.diag(
+            torch.diag(torch.ones(action_dim, action_dim))).unsqueeze(0)
+
+        self._output_spec = TensorSpec(())
+
+    def forward(self, inputs, state=()):
+        """Computes action-value given an observation.
+
+        Args:
+            inputs:  A tuple of Tensors consistent with `input_tensor_spec`
+            state: empty for API consistent with CriticRNNNetwork
+
+        Returns:
+            action_value (torch.Tensor): a tensor of the size [batch_size]
+            state: empty
+        """
+        # this line is just a placeholder doing nothing
+        inputs, state = Network.forward(self, inputs, state)
+
+        observations, actions = inputs
+        actions = actions.to(torch.float32)
+
+        # 0 encode observation
+        encoded_obs, _ = self._obs_encoder(observations)
+
+        # 1 mu
+        mu, _ = self._mu(encoded_obs)
+
+        # 2 V
+        V, _ = self._V(encoded_obs)
+
+        # 3 Q
+        Q = None
+        if actions is not None:
+            num_outputs = mu.size(1)
+            L = self.L(encoded_obs).view(-1, num_outputs, num_outputs)
+            L = L * \
+                self.tril_mask.expand_as(
+                    L) + torch.exp(L) * self.diag_mask.expand_as(L)
+            P = torch.bmm(L, L.transpose(2, 1))
+
+            u_mu = (actions - mu).unsqueeze(2)
+            A = -0.5 * \
+                torch.bmm(torch.bmm(u_mu.transpose(2, 1), P), u_mu)[:, :, 0]
+
+            Q = A + V
+
+        return mu, Q, V
